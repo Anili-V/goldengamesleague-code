782f751ea12d64f059c3d58d60e81169a77a81c6
Anili-V <40320684+Anili-V@users.noreply.github.com>
2018-06-30 23:54:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 	<title>GGL</title>
 </head>
 <H1 id="gv">ABOUT US</H1><br>
-<h1 id="gh"><B>How this all started ?</B></h1>
+<h1 id="gi"><B><center>How this all started ?</center></B></h1>
 <h2 id="gi">On getting inspiration from Narendra Modi Ji's "Mann Ki Baat" serial on TV GGL was formed with the vision of making a website to make Indian youngsters fit. There was one lady, an Indian citizen who shared her thoughts with Modi Ji in the serial. She asked him that what will he do to take away phones from youngsters hands and encourage them to play. In today's time this is a big question for all parents, teachers and youngsters too. Sports is a good alternative to mobiles and other screens.</h2><br><br><br>
-<H1 id="go">Aim</H1>
+<H1 id="gi"><center>Aim</center></H1>
 <h2 id="gi">Golden Games League(GGL) aims to create a common structure for urban and rural India for competition across sports. It also aims to promote participation from all Indian youngsters irrespective of economic & financial status, sex, literacy, location etc. The games which will be played on this platform are not regular games like cricket, football, basketball, tennis but are gully games like pithoo, gallery, dog-in-the-bone etc. These are long forgotton old games. Old games are Golden games. Hence the name, Golden Games League.   </h2>
 <?php include 'footer.php'; ?>
